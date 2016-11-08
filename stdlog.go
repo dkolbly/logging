08d38e7a1fce5b2d8245a8ed7fa19a68129e6e19
Donovan Kolbly <donovan@rscheme.org>
2016-11-08 13:48:43 -0600

    Fix log.Fatal() api

--- stdlog.go
+++ stdlog.go
@@ -1,13 +1,14 @@
 package logging
 
 import (
+	"bytes"
 	"os"
 )
 
 // these implement log.Logger for *Logger
 
-func (l *Logger) Fatal(format string, args ...interface{}) {
-	l.dispatch(format, args, CRITICAL, baseDepth)
+func (l *Logger) Fatal(args ...interface{}) {
+	l.dispatch(synthesizeFormat(args), args, CRITICAL, baseDepth)
 	os.Exit(1)
 }
 
@@ -16,3 +17,21 @@ func (l *Logger) Fatalf(format string, args ...interface{}) {
 	os.Exit(1)
 }
 
+func synthesizeFormat(args []interface{}) string {
+	if len(args) == 1 {
+		return "%v"
+	}
+	var buf bytes.Buffer
+	// pretend like it was a string so we don't add a space
+	wasString := true
+	for _, a := range args {
+		_, isStr := a.(string)
+		if !(wasString && isStr) {
+			buf.WriteByte(' ')
+		}
+		buf.WriteByte('%')
+		buf.WriteByte('v')
+		wasString = isStr
+	}
+	return buf.String()
+}
